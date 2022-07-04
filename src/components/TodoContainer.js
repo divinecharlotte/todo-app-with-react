a64c10f1fc516350e353a300432fe03a6e3ae0f2
charlotte-divine <divinemaina@gmail.com>
2022-07-04 20:44:38 +0100

    project set up

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,81 @@
+import React from "react"
+import TodosList from "./TodosList";
+import Header from "./Header";
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
+// import { Route, Switch } from "react-router-dom"
+class TodoContainer extends React.Component {
+  state = {
+  todos: []
+}
+    handleChange = id => {
+      this.setState(prevState => ({
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        }),
+      }))
+    };
+  delTodo = id => {
+  this.setState({
+    todos: [
+      ...this.state.todos.filter(todo => {
+        return todo.id !== id;
+      })
+    ]
+  });
+};
+addTodoItem = title => {
+  const newTodo = {
+    id: uuidv4(),
+    title: title,
+    completed: false
+  };
+  this.setState({
+    todos: [...this.state.todos, newTodo]
+  });
+};
+setUpdate = (updatedTitle, id) => {
+  this.setState({
+    todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.title = updatedTitle
+      }
+      return todo
+    }),
+  })
+}
+componentDidMount() {
+ fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    .then(response => response.json())
+    .then(data => this.setState({ todos: data }));
+}
+// componentDidUpdate(prevProps, prevState) {
+//   if(prevState.todos !== this.state.todos) {
+//     const temp = JSON.stringify(this.state.todos)
+//     localStorage.setItem("todos", temp)
+//   }
+// }
+   render() {
+    return (
+      <div className="container">
+      <div className="inner">
+          < Header/>
+          <TodosList
+  todos={this.state.todos}
+  handleChangeProps={this.handleChange}
+  deleteTodoProps={this.delTodo}
+  setUpdate={this.setUpdate}
+/>
+          <InputTodo addTodoProps={this.addTodoItem} />
+        </div>
+          </div>
+    );
+  }
+}
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/TodoContainer';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./components/TodoContainer"
+import "./App.css"
+import { BrowserRouter } from "react-router-dom"
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+)

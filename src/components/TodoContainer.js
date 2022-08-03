218848a067e3740e025448289a006bf1ef98e565
charlotte-divine <divinemaina@gmail.com>
2022-08-03 14:51:18 +0100

    add all functionalities

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
-// import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component { 
   state = {
@@ -61,16 +61,16 @@ class TodoContainer extends React.Component {
    
   };
 
-  // addTodoItem = (title) => {
-  //   const newTodo = {
-  //     id: uuidv4(),
-  //     title: title,
-  //     completed: false,
-  //   };
-  //   this.setState({
-  //     todos: [...this.state.todos, newTodo],
-  //   });
-  // };
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
   setUpdate = (updatedTitle, id) => {
     this.setState({
       todos: this.state.todos.map((todo) => {
